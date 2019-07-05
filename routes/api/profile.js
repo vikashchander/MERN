7644415a414a043e86bbe6d90032e68a00c7e6c9
vikash chander <vikashchander78384@gmail.com>
2019-07-05 11:52:34 +0530

    get current user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,13 +1,29 @@
 const express = require('express'),
-    routes = express.Router();
+    routes = express.Router(),
+    auth = require('../../middleware/auth'),
+    profile = require('../../models/profile'),
+    user = require('../../models/Users');
 
 // @routes Get  api/profile
 // @des        Test route
 //  @access    Public 
 
 
-routes.get('/', (req, res) => {
-    res.send("profile routes")
+routes.get('/me', auth, async (req, res) => {
+    try {
+        const data = profile.findOne({
+            user: req.userData.id
+        }).populate('user', ['name', 'avator']);
+        if (!data) {
+            return res.status(400).json({
+                msg: ' no user profile data found'
+            })
+        }
+        res.status(200).json(data);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
 })
 
 module.exports = routes;
